8e612c6dbf3fb94aee2c3e31a349dad9e722cdf1
Shaun Irwin <shaun.irwin@gmail.com>
2025-06-10 20:56:17 +0200

    Add dataset fixture

--- signwriting_animation/diffusion/tests/test_dataset.py
+++ signwriting_animation/diffusion/tests/test_dataset.py
@@ -1,8 +1,12 @@
+import pytest
+
 from signwriting_animation.data.data_loader import DynamicPosePredictionDataset
 
-def test_get_batch():
-    num_past_frames_max = 40
-    num_future_frames_max = 20
+
+@pytest.fixture(scope="module")
+def pose_dataset():
+    num_past_frames_max = 8
+    num_future_frames_max = 5
     num_keypoints = 586
     num_dims_per_keypoint = 3
 
@@ -14,6 +18,11 @@ def test_get_batch():
         with_metadata=True,
         split='train'
     )
+    return dataset, num_past_frames_max, num_future_frames_max, num_keypoints, num_dims_per_keypoint
+
+
+def test_get_batch_from_dataset(pose_dataset):
+    dataset, num_past_frames_max, num_future_frames_max, num_keypoints, num_dims_per_keypoint = pose_dataset
 
     batch = dataset[0]
 
@@ -28,3 +37,5 @@ def test_get_batch():
     assert batch['conditions']['input_mask'].shape == (num_past_frames_sel, 1, num_keypoints, num_dims_per_keypoint)
     assert batch['conditions']['target_mask'].shape == (num_future_frames_sel, 1, num_keypoints, num_dims_per_keypoint)
     assert batch['conditions']['sign_image'].shape == (3, 224, 224)
+
+    assert batch['id'] == 'whatsthatsign-524.pose'
