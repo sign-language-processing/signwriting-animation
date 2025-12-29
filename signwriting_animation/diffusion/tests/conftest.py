4bdc212afad927b657b0cdc6caaf6fe383446070
Yang Yun <yang.yun@uzh.ch>
2025-12-30 00:07:28 +0100

    Update conftest.py

--- signwriting_animation/diffusion/tests/conftest.py
+++ signwriting_animation/diffusion/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-from signwriting_animation.data.data_loader import DatasetConfig, DynamicPosePredictionDataset
+from signwriting_animation.data.data_loader import DynamicPosePredictionDataset
+
 
 @pytest.fixture(scope="module")
 def pose_dataset():
@@ -12,15 +13,13 @@ def pose_dataset():
     num_keypoints = 586
     num_dims_per_keypoint = 3
 
-    config = DatasetConfig(
+    dataset = DynamicPosePredictionDataset(
         data_dir='signwriting_animation/data/test_data/pose_samples',
         csv_path='signwriting_animation/data/test_data/transcription_samples/data_subset.csv',
         num_past_frames=num_past_frames_max,
         num_future_frames=num_future_frames_max,
+        with_metadata=False,
         split='train'
     )
-    dataset = DynamicPosePredictionDataset(
-        config=config,
-        with_metadata=False
-    )
+
     return dataset, num_past_frames_max, num_future_frames_max, num_keypoints, num_dims_per_keypoint
